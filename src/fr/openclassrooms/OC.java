5e295c2d8b46ca81188a29ec55c4e8be98ea467a
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-05-10 19:28:26 +0200

    OC.java: create varaible exempleStr with method of class String toUppecase() and replace()

---
+++ src/fr/openclassrooms/OC.java
@@ -0,0 +1,12 @@
+package fr.openclassrooms;
+
+public class OC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String exempleStr = "hello";
+		exempleStr = exempleStr.toUpperCase();
+		exempleStr = exempleStr.replace("HELL", "YEAH");
+		System.out.println(exempleStr);
+	}
+}
