0bbb558f0c8ad3fe22a12c89f69320da16e16198
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-05-10 15:02:21 +0200

    create package hello and class HelloWorld.java

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Ceci est une implémentation du message traditionnel "Hello world!"
+ * 
+ * @author L'équipe Education d'OpenClassrooms
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
