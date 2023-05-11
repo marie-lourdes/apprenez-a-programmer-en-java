5d829a3ca3d0b2999c1f975a7c11f94a9dd55f9f
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-05-11 14:09:52 +0200

    BoucleJava.java: create boucle for in main() call metho bonjour, and use class interger to string arg oh the methode

--- src/fr/openclassrooms/BoucleFor.java
+++ src/fr/openclassrooms/BoucleFor.java
@@ -5,8 +5,10 @@ public class BoucleFor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 1; i <= 1000; i++) {
+			// Integer permet d utilser ces methodes sur un integer, ici i est integer nous
+			// convertissons en string
+			// car la methode bonjour recoit en argument un type String
 			bonjour("jean " + Integer.toString(i));
-
 		}
 	}
 
